6860ea7d6dad3593e224b9a327f31756c568a787
RaissaMariaB <raissamaria.sb@gmail.com>
2019-10-01 13:58:36 -0300

    Adiciona validação de texto e limpar input ao recarregar

--- JS/script.js
+++ JS/script.js
@@ -10,18 +10,44 @@ formulario.addEventListener("submit", function(evento){
     let digitarTarefas = document.getElementById("formulario-input")
     let guardarValorTaf = digitarTarefas.value.trim()
 
+        if (guardarValorTaf === ""){
+            digitarTarefas.setAttribute("placeholder", "Digite tarefa válida, senhora.")
+            
+        }else  { 
+
+
         let listarTarefas = document.createElement("div")
+        listarTarefas.setAttribute("class", "lista__tarefa")
         boxTarefas.appendChild(listarTarefas)
         let mostrarTarefas = document.createElement("p")
         listarTarefas.appendChild(mostrarTarefas)
         let cancel = document.createElement("button")
+        cancel.setAttribute ("class", "botao_x")
         cancel.textContent = "x"
         listarTarefas.appendChild(cancel)
 
         mostrarTarefas.textContent = guardarValorTaf
+        digitarTarefas.getAttribute("placeholder")
+
         
-        formulario.reset ()
+    }
+   
+    formulario.reset ()
+    mostrarTarefas.addEventListener("click", function(evento){
+    if(mostrarTarefas.classList.contains("riscar__tarefa")){
+        mostrarTarefas.classList.remove("riscar__tarefa")
+
+    } else{
+        mostrarTarefas.setAttribute("class","riscar__tarefa")
+
+    }
     
+    
+    
+    
+    
+    })
+
 })
 
 
